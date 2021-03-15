4723e3ce81f9c36fc1f0167987c34ac8c03479b0
Abhay Pratap <63657843+sgnabhay@users.noreply.github.com>
2021-03-15 19:52:24 +0530

    Add files via upload

---
+++ guessinggame.sh
@@ -0,0 +1,20 @@
+function guessloop {
+  while [[ $response -ne $correct_number ]]
+  do 
+    if [[ $response -lt $correct_number ]]
+    then
+      echo "Your guess is too low. Please enter a number again:"
+      read response
+    elif [[ $response -gt correct_number ]]
+    then
+      echo "Your guess is too high. Please enter a number again:"
+      read response
+    fi
+  done
+  echo "Congratulations! Your answer is correct!"
+}
+
+echo "Plese enter how many files are in the current directory:"
+read response
+correct_number=$(ls | wc -l)
+guessloop response correct_number
